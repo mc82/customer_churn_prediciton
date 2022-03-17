80680f01a8e6e937750ed693f68ef94a2e26efd5
Maik Götze <maik.goetze@scout24.com>
2022-03-17 11:08:28 +0100

    optimize pylint score

--- test/classifier/test_logistic_regression.py
+++ test/classifier/test_logistic_regression.py
@@ -1,18 +1,45 @@
-import pytest
-from pathlib import Path
+"""Implements tests of logistic_regression module
+"""
 import pickle
+from pathlib import Path
+
+from pandas import DataFrame
+import pytest
+
 from classifier import LogisticRegression
 
 ARTIFACT_MODEL_PATH = "test/artifacts/data/model/logistic_regression.pkl"
 
 
 @pytest.fixture
-def logistic_regression(tmpdir):
+def logistic_regression(tmpdir) -> LogisticRegression:
+    """Creates object of classifier
+
+    Args:
+        tmpdir: Tmp directory to init classifier
+
+    Returns:
+        LogisticRegression: Initialized estimator
+    """
     return LogisticRegression(model_path=tmpdir)
 
 
 @pytest.fixture
-def logistic_regression_trained(logistic_regression, X_train, y_train):
+def logistic_regression_trained(
+        logistic_regression: LogisticRegression,
+        X_train: DataFrame,
+        y_train: DataFrame
+) -> LogisticRegression:
+    """Fixture to create a trained classifier
+
+    Args:
+        logistic_regression (LogisticRegression): Classifier to train
+        X_train (DataFrame):  Dependent data set to fit the model
+        y_train (DataFrame):  Independent data set to fit the model
+
+    Returns:
+        LogisticRegression: Fitted classifier with random data
+    """
     model_path = ARTIFACT_MODEL_PATH
     _model_path = Path(model_path)
     if _model_path.is_file():
@@ -27,14 +54,27 @@ def logistic_regression_trained(logistic_regression, X_train, y_train):
 
 @pytest.mark.classifier
 @pytest.mark.logistic_regression
-def test_fit(logistic_regression, X_train, y_train):
+def test_fit(
+    logistic_regression: LogisticRegression,
+    X_train: DataFrame,
+    y_train: DataFrame
+):
+    """Test fitting the model with reandom data
+
+    Args:
+        logistic_regression (LogisticRegression): _description_
+        X_train (DataFrame): Dependent data set
+        y_train (DataFrame): Independent data set
+    """
     logistic_regression.fit(X=X_train, y=y_train)
     assert logistic_regression._model is not None
 
 
 @pytest.mark.classifier
 @pytest.mark.logistic_regression
-def test_load():
+def test_load() -> None:
+    """Test loading a pickled model
+    """
     logistic_regression = LogisticRegression(model_path=ARTIFACT_MODEL_PATH)
     logistic_regression.load()
     assert logistic_regression._model is not None
@@ -42,7 +82,14 @@ def test_load():
 
 @pytest.mark.classifier
 @pytest.mark.logistic_regression
-def test_save(logistic_regression_trained, tmpdir):
+def test_save(logistic_regression_trained: LogisticRegression, tmpdir) -> None:
+    """_summary_
+
+    Args:
+        logistic_regression_trained (LogisticRegression): Fitted estimator
+        to be ready for saving
+        tmpdir :  Tmp directory to save model
+    """
     logistic_regression_trained._model_path = tmpdir + "/loistic_regression_model.pkl"
     logistic_regression_trained.save()
     assert len(tmpdir.listdir()) == 1
--- test/conftest.py
+++ test/conftest.py
@@ -1,18 +1,31 @@
+"""Implements fixtures to be used in multiple test modules
+"""
 import numpy as np
-import pytest
 import pandas as pd
+from pandas import DataFrame
+import pytest
 
 NUMBER_OF_TEST_SAMPLES = 100
 BANK_DATA_PATH = "/Users/mgoetze/repos/customer_churn_prediction/test/artifacts/bank_data.csv"
 
 
 @pytest.fixture
-def bank_data():
+def bank_data() -> DataFrame:
+    """Fixture to deliver bank data
+
+    Returns:
+        DataFrame: Bank data from CSV file
+    """
     return pd.read_csv(BANK_DATA_PATH)
 
 
 @pytest.fixture
 def X_train() -> pd.DataFrame:
+    """Set of dependent data to train a classifier
+
+    Returns:
+        pd.DataFrame: Random values
+    """
     return pd.DataFrame(
         {
             "a": np.random.uniform(size=NUMBER_OF_TEST_SAMPLES),
@@ -21,16 +34,37 @@ def X_train() -> pd.DataFrame:
     )
 
 
-def X_test(X_train) -> pd.DataFrame:
+def X_test(X_train: DataFrame) -> DataFrame:
+    """Replicates training data
+
+    Args:
+        X_train (DataFrame): Data coming from another fixture
+
+    Returns:
+        DataFrame: Random values
+    """
     return X_train
 
 
 @pytest.fixture
-def y_train() -> pd.DataFrame:
+def y_train() -> DataFrame:
+    """Independet variable
+
+    Returns:
+        DataFrame: Random int values to train / verify classifier
+    """
     return pd.DataFrame({"y": np.random.randint(
         low=0, high=2, size=NUMBER_OF_TEST_SAMPLES)})
 
 
 @pytest.fixture
-def y_test(y_train):
+def y_test(y_train: DataFrame) -> DataFrame:
+    """Replicates the data of another fixture
+
+    Args:
+        y_train (DataFrame): 1-dim data
+
+    Returns:
+        DataFrame: Random int values to train / verify classifier
+    """
     return y_train
