bf523daa7402f0fc433622084576a243aec34b6c
Maik Götze <maik.goetze@scout24.com>
2022-04-07 10:11:17 +0200

    remove logging form Directory bc of circular imports

--- src/directory.py
+++ src/directory.py
@@ -3,8 +3,6 @@ __author__ = "Maik Goetze"
 """
 from pathlib import Path
 
-from logger import logging
-
 
 class Directory:
     """Implements logic to create a directory if not exists and returns the path to it
@@ -20,7 +18,7 @@ class Directory:
 
         :return: Path of the directory
         :rtype: Path
-        """        
+        """
         if not self._check_existence():
             self._create()
         return self._path
@@ -29,15 +27,11 @@ class Directory:
         """Checks existens of directory
         :return: returns True if directory exists otherwise False
         :rtype: bool
-        """        
-        logging.info("Checking existence of directory {}".format(
-            self._directory_name))
+        """
         exists = self._path.is_dir()
-        logging.info("Directory exists: {}".format(exists))
         return exists
 
     def _create(self) -> None:
         """Creates directory
         """
-        logging.info("Creating directory {}".format(self._directory_name))
         Path(self._path).mkdir(parents=True, exist_ok=True)
